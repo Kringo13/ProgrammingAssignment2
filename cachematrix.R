f6a18699042e646380ead3cd043262e8ad958947
Kringo13 <kurtringo13@gmail.com>
2021-02-08 15:40:34 -0500

    Mostly replace from example with "solve" instead of "mean"

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function to demonstrate Lexical Scoping
+## REturn cached value of matrix inverse, if exists
 
-## Write a short comment describing this function
+## create special vector of matrix inverse using gets and sets
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+    minv <- NULL
+    set <- function(y) {
+      x <<- y
+      minv <<- NULL
+    }
+    get <- function() x
+    setminv <- function(solve) minv <<- solve
+    getminv <- function() solve
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+  
 }
 
 
-## Write a short comment describing this function
+## Calculates inverse of MakeCacheMatrix.  If already exists, replace with known values from Globale Envirn.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  minv <- x$getminv()
+  if(!is.null(minv)) {
+    message("getting cached data")
+    return(minv)
+  }
+  data <- x$get()
+  minv <- solve(data, ...)
+  x$setminv(minv)
+  minv
 }
